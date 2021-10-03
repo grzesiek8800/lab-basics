0b4297c13e4a8dce68dc0969e6a62a9944fd2059
gkurek <grzesiek8800@gmail.com>
2021-10-03 14:44:40 +0200

    first steps

---
+++ src/main/java/org/example/pr1/Myclass.java
@@ -0,0 +1,16 @@
+package org.example.pr1;
+
+public class Myclass {
+
+    public String main1;
+    protected String main2;
+    String main3;
+    private String main4;
+
+    public void print(){
+        System.out.println("main1: "+main1);
+        System.out.println("main2: "+main2);
+        System.out.println("main3: "+main3);
+        System.out.println("main4: "+main4);
+    }
+}
---
+++ src/main/java/org/example/pr1/myclass/MyextendedClass.java
@@ -0,0 +1,8 @@
+package org.example.pr1.myclass;
+
+import org.example.pr1.Myclass;
+
+public class MyextendedClass extends Myclass {
+
+
+}
